911a60623201d68bbdc214f6ae665ac4e51175c3
shohedul1 <shohedulislam564@gmail.com>
2023-09-02 21:57:25 +0600

    frist-commit2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import React from 'react';
+import Head from 'next/head';
+import SiteHeader from '../components/SiteHeader';
+import Link from 'next/link';
+import Index from './page';
+
 
 
 export default function Home() {
+ 
   return (
     <>
-    <h1>hello world</h1>
+     <Head>
+      <title key="pagetitle">Welcome to collNomad Travel Blog</title>
+      <meta name='description' content='coolnomad travel blog - read our travel stories' key="metadescription"/>
+     </Head>
+     <div className="min-h-screen bg-[url('/home.jpg')]  bg-cover relative">
+        <div className='absolute bg-slate-900 inset-0 z-0 opacity-40'></div>
+        <SiteHeader className='z-10 relative' />
+        <main>
+          <div className='min-h-[50vh] flex flex-col items-center justify-center z-10 relative'>
+            <h1 className='text-6xl text-center text-slate-100'>Welcome to <span className='text-yellow-400'> CoolNomad</span> Travel Blog</h1>
+            <div className='mt-8'>
+              <Link href='/blog' className='bg-slate-100 border py-3 px-4 hover:bg-yellow-400 transition'>Read Blog</Link>   
+            </div>  
+          </div>
+        </main>
+     </div>
     
+    
+     
     </>
+   
+   
   )
 }
